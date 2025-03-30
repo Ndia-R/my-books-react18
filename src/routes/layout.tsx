02ed4eb38aa34a4c0dd7d36b4a23f44732e0fbfe
ubuntu <ubuntu@gmail.com>
2025-03-30 04:29:42 +0000

    refactoring

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -10,8 +10,8 @@ export default function Layout() {
       <div className="fixed inset-0 -z-50 min-h-dvh bg-gradient-to-tr from-primary/10 via-background via-80% to-primary/15"></div>
 
       <div className="min-h-dvh">
-        <Header className="fixed z-50 w-screen" />
-        <main className="mx-auto max-w-7xl px-3 pt-16 sm:px-6">
+        <Header className="sticky top-0 z-50 w-full" />
+        <main className="mx-auto max-w-7xl px-3 sm:px-6">
           <Outlet />
         </main>
         <Footer className="sticky top-full" />
